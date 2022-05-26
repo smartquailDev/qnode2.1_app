a4f1445beee9895f9b41b9702145a3b09b90eb89
smartquailDev <smartquail.dev@gmail.com>
2022-05-26 12:37:39 -0500

    Update views.py

--- qnode21_app/account/views.py
+++ qnode21_app/account/views.py
@@ -87,11 +87,6 @@ def user_login(request):
 def dashboard(request):
     # Display all actions by default
     actions = Action.objects.exclude(user=request.user)
-    following_ids = request.user.following.values_list('id',
-                                                       flat=True)
-    if following_ids:
-        # If user is following others, retrieve only their actions
-        actions = actions.filter(user_id__in=following_ids)
     actions = actions.select_related('user', 'user__profile')\
                      .prefetch_related('target')[:10]
 
